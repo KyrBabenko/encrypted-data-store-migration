304eba4ac3ae4929276d4c4ae9c144d014708a86
kyrylo.babenko <kyr.babenko@gmail.com>
2025-01-20 22:25:47 +0200

    Remove edgeToEdge

--- app/src/main/java/com/example/migration/MainActivity.kt
+++ app/src/main/java/com/example/migration/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.migration
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.example.migration.databinding.ActivityMainBinding
@@ -34,15 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         binding.title.text = sharedPreferences.getString(TITLE_KEY, null) ?: ""
         binding.encrypt.setOnClickListener {
             val value = binding.editText.text.toString()
