0a2f21b568d93879125a0d606f2e5f705643b896
kyrylo.babenko <kyr.babenko@gmail.com>
2025-01-20 22:28:07 +0200

    Improve codestyle

--- app/src/main/java/com/example/migration/preferences/Preferences.kt
+++ app/src/main/java/com/example/migration/preferences/Preferences.kt
@@ -1,8 +1,6 @@
 package com.example.migration.preferences
 
 interface Preferences {
-
     fun getString(key: String, defaultValue: String?): String?
-
     fun putString(key: String, value: String)
 }
